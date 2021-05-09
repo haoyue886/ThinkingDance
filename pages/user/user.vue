13cc7e878c58682690fd14cf2a0b3bcb8feffd46
haoyue <1762351793@qq.com>
2021-05-09 17:54:33 +0800

    第一次提交

---
+++ pages/user/user.vue
@@ -0,0 +1,15 @@
+<template>
+	<view class="t-icon iconhuodong">引用字体图标</view>
+</template>
+<script>
+</script>
+
+<style>
+	.icon {
+	  width: 1em;
+	  height: 1em;
+	  vertical-align: -0.15em;
+	  fill: currentColor;
+	  overflow: hidden;
+	}
+</style>
